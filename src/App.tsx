f2499fca62e3d6f3772e587da42e8678a29214c7
Grassparker <candy20190715@outlook.com>
2026-05-10 22:26:32 +0800

    Change

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css'
+import { GoogleGenAI } from "@google/genai";
 
 function App() {
   return (
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { GoogleGenAI } from "@google/genai";
 import './index.css'
 import App from './App.tsx'
 
